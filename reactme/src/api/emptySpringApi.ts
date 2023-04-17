48c0881eaf98f8f7a4ec3488dae65357045677f3
Juje <msk990@yahoo.com>
2023-04-17 18:40:25 +0300

    discovered I needed to hardcode the IP. Whaaat?

--- reactme/src/api/emptySpringApi.ts
+++ reactme/src/api/emptySpringApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 // initialize an empty api service that we'll inject endpoints into later as needed
 export const springApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: `http://juje:8080/api/v1`}),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_URL}),
   endpoints: () => ({}),
   reducerPath: 'sqlApi',
   tagTypes: ['items', 'foods', 'meals']
